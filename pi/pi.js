62daacbb9be79ef511ebb2aa6cf65f49d27adb61
DrDesten <>
2022-03-16 00:22:01 +0100

    added some styling, aded input field

--- pi/pi.js
+++ pi/pi.js
@@ -2,3 +2,9 @@ const PiMioURL = "https://raw.githubusercontent.com/DrDesten/web/main/pi/PiMio.t
 
 var PiHTML = document.getElementById("pi")
 PiHTML.innerHTML = "3.1415926535897932384626433832795028841971693993751058209749445923078164"
+//PiHTML.innerHTML = "3.14159"
+
+
+function printText(txt) {
+    console.log(txt)
+}

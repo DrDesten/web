89034f2a1fe589483fdb448a0cbf20ff0ae30574
DrDesten <>
2022-03-16 01:00:40 +0100

    added some rudimentary searching

--- pi/pi.js
+++ pi/pi.js
@@ -1,10 +1,27 @@
 const PiMioURL = "https://raw.githubusercontent.com/DrDesten/web/main/pi/PiMio.txt"
 
+var piString = "3.1415926535897932384626433832795028841971693993751058209749445923078164"
+
 var PiHTML = document.getElementById("pi")
-PiHTML.innerHTML = "3.1415926535897932384626433832795028841971693993751058209749445923078164"
-//PiHTML.innerHTML = "3.14159"
+PiHTML.innerHTML = piString
+
+fetch(PiMioURL)
+   .then( request => request.text() )
+   .then( text =>    piString = text )
 
 
-function printText(txt) {
-    console.log(txt)
+function searchPi(text = "") {
+    text = text.replace(/[^0-9]/g, "")
+    if (text.length == 0) text = "abc" // Instantly causes null
+    
+    var match = piString.match(text)
+    if (match == null) return
+
+    var index = match.index
+    var displayString = piString.slice(Math.max(index - 100, 0), Math.min(index + 100, piString.length))
+
+    PiHTML.innerHTML = displayString
+    PiHTML.style.transform = "translateX(-50%)"
+
+    console.log(displayString)
 }

85659c38993d4aa99a6e76d34bacadc513411630
DrDesten <>
2022-03-24 23:45:46 +0100

    Made A functioning sample optimizer Implemented improveProgressiveness1D and 2D functions. They allow you to resort your array so that the samples are distributed progressively. This means that the samples are in a uniform distribution, no matter where in the array you start and how many samples you take

--- tools/sample_optimizer/sample_optimizer.js
+++ tools/sample_optimizer/sample_optimizer.js
@@ -57,7 +57,7 @@ const chart = new Chart(ctx, config)
 
 
 // ACTUAL CALCULATIONS //////////////////////////////////////////////////////////////////////////
-
+/* 
 function judgeProgressiveness1D(sample_arr) {
     squareDeviation = []
 
@@ -153,4 +153,135 @@ function runOptimizer(array, steps = 10) {
 
     return optimizedArray
 }
+ */
 
+function calcAverage(arr) { return arr.reduce((sum, curr) => sum + curr, 0) / arr.length }
+function calcVariance(arr, average) { return arr.reduce((sum, curr) => sum + ((curr - average)*(curr - average)), 0) / arr.length }
+
+function judgeProgressiveness1D(arr = []) {
+
+    let averages = [], variances = []
+    
+    for (let arrLength = 2; arrLength < arr.length; arrLength++) {
+
+        for (let position = 0; position < arr.length - arrLength; position++) {
+
+            sliceToAnalyze = arr.slice(position, position + arrLength)
+            let average = calcAverage(sliceToAnalyze)
+            averages.push(average)
+            variances.push(calcVariance(sliceToAnalyze, average))
+
+        }
+
+    }
+
+    let progressiveness = calcVariance(averages, calcAverage(averages)) + calcVariance(variances, calcAverage(variances))
+    progressiveness     = 1 / (progressiveness + 1);
+    return progressiveness
+}
+
+function calcAverage2D(arr) { average = arr.reduce((sum, curr) => [sum[0]+curr[0],sum[1]+curr[1]], [0,0]); return [average[0]/arr.length, average[1]/arr.length] }
+function calcVariance2D(arr, average) { variance = arr.reduce((sum, curr) => [sum[0]+((curr[0]-average[0])*(curr[0]-average[0])), sum[1]+((curr[1]-average[1])*(curr[1]-average[1]))], [0,0]); return [variance[0]/arr.length, variance[1]/arr.length] }
+
+function judgeProgressiveness2D(arr = [[]]) {
+
+    let averages = [], variances = []
+
+    for (let arrLength = 2; arrLength <= arr.length; arrLength++) {
+
+        for (let position = 0; position <= arr.length - arrLength; position++) {
+
+            sliceToAnalyze = arr.slice(position, position + arrLength)
+            average = calcAverage2D(sliceToAnalyze)
+            averages.push(average)
+            variances.push(calcVariance2D(sliceToAnalyze, average).reduce((sum, curr) => sum+curr, 0)) // Sum up both x and y variances
+
+        }
+
+    }
+
+    //console.log(averages, variances)
+
+    let progressiveness = calcVariance2D(averages, calcAverage2D(averages)).reduce((sum, curr) => sum+curr, 0) + calcVariance(variances, calcAverage(variances))
+    progressiveness     = 1 / (progressiveness + 1);
+    return progressiveness
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function randInt(from = 0, to = 1) { // Returns a random Integer from from (including) to to (excluding)
+    return Math.round(Math.random() * (to - from - 1)) + from
+}
+
+function improveProgressiveness1D(arr = [], steps = 1000) {
+
+    let progressiveness
+    for (let i = 0; i < steps; i++) {
+
+        progressiveness = judgeProgressiveness1D(arr)
+
+        let pos1 = randInt(0, arr.length)
+        let pos2 = randInt(0, arr.length)
+        
+        let tmp = arr[pos1]
+        arr[pos1] = arr[pos2]
+        arr[pos2] = tmp
+
+        let newProgressiveness = judgeProgressiveness1D(arr)
+
+        // If the progressiveness improved, keep array and set the new progressiveness
+        // If not, swap back
+        if (newProgressiveness > progressiveness) {
+            progressiveness = newProgressiveness
+        } else {
+            let tmp = arr[pos1]
+            arr[pos1] = arr[pos2]
+            arr[pos2] = tmp
+        }
+
+    }
+
+    return arr
+
+}
+
+function improveProgressiveness2D(arr = [[]], steps = 100) {
+
+    let progressiveness
+    for (let i = 0; i < steps; i++) {
+
+        progressiveness = judgeProgressiveness2D(arr)
+
+        let pos1 = randInt(0, arr.length)
+        let pos2 = randInt(0, arr.length)
+        
+        let tmp = arr[pos1]
+        arr[pos1] = arr[pos2]
+        arr[pos2] = tmp
+
+        let newProgressiveness = judgeProgressiveness2D(arr)
+
+        // If the progressiveness improved, keep array and set the new progressiveness
+        // If not, swap back
+        if (newProgressiveness > progressiveness) {
+            progressiveness = newProgressiveness
+        } else {
+            let tmp = arr[pos1]
+            arr[pos1] = arr[pos2]
+            arr[pos2] = tmp
+        }
+
+    }
+
+    return arr
+
+}

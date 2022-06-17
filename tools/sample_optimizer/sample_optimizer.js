1bb4a63cc750052090ca9a5593f3fc1c226561ca
DrDesten <>
2022-06-17 12:54:51 +0200

    sample optimizer better behaviour when steps=0

--- tools/sample_optimizer/sample_optimizer.js
+++ tools/sample_optimizer/sample_optimizer.js
@@ -1,22 +1,22 @@
-const goldenAngle      = Math.PI * (3 - Math.sqrt(5))
-const goldenAngleRcp   = 1.0 / goldenAngle
-const goldenAngleRcpSq = goldenAngleRcp*goldenAngleRcp
+const goldenAngle = Math.PI * ( 3 - Math.sqrt( 5 ) )
+const goldenAngleRcp = 1.0 / goldenAngle
+const goldenAngleRcpSq = goldenAngleRcp * goldenAngleRcp
 
-function convert2DtoChart(arr) {
-    return arr.map(e => { return {x: e[0], y: e[1]} })
+function convert2DtoChart( arr ) {
+    return arr.map( e => { return { x: e[0], y: e[1] } } )
 }
 
 
 // PREVIEW CHART //////////////////////////////////////////////////////////////////////////
 
-const ctx = document.getElementById("preview_chart")
+const ctx = document.getElementById( "preview_chart" )
 
 let data = {
     datasets: [{
         label: 'Sample Preview',
-        data: [{x:0,y:0}],
+        data: [{ x: 0, y: 0 }],
     }],
-};
+}
 
 let config = {
     type: 'scatter',
@@ -47,72 +47,72 @@ let config = {
             point: {
                 radius: 7.5,
                 borderWidth: 0,
-                backgroundColor: function(context) {
-                    let pos = context.dataIndex / (context.dataset.data.length - 1);
+                backgroundColor: function ( context ) {
+                    let pos = context.dataIndex / ( context.dataset.data.length - 1 )
                     return `rgb(${pos * 255}, ${pos * 255}, ${pos * 255})`
                 },
             }
         }
     }
-};
+}
 
-const chart = new Chart(ctx, config)
+const chart = new Chart( ctx, config )
 
-function updateChart(valueArray = []) {
-    chart.data.datasets[0].data = valueArray.map(val => { return {x: val[0], y: val[1]} });
-    chart.update();
+function updateChart( valueArray = [] ) {
+    chart.data.datasets[0].data = valueArray.map( val => { return { x: val[0], y: val[1] } } )
+    chart.update()
 }
 
 
 
 // ACTUAL CALCULATIONS //////////////////////////////////////////////////////////////////////////
 
-function calcAverage(arr) { return arr.reduce((sum, curr) => sum + curr, 0) / arr.length }
-function calcVariance(arr, average) { return arr.reduce((sum, curr) => sum + ((curr - average)*(curr - average)), 0) / arr.length }
+function calcAverage( arr ) { return arr.reduce( ( sum, curr ) => sum + curr, 0 ) / arr.length }
+function calcVariance( arr, average ) { return arr.reduce( ( sum, curr ) => sum + ( ( curr - average ) * ( curr - average ) ), 0 ) / arr.length }
 
-function calcVarianceSqrtWeight(arr, average) { return arr.reduce((sum, curr, i) => sum + (Math.sqrt(i+1)*(curr - average)*(curr - average)), 0) / arr.length }
+function calcVarianceSqrtWeight( arr, average ) { return arr.reduce( ( sum, curr, i ) => sum + ( Math.sqrt( i + 1 ) * ( curr - average ) * ( curr - average ) ), 0 ) / arr.length }
 // The respective scaling factor wouldn't actually be 1/arr.length but arr.length**1.5/1.5 - 1/1.5, but it doesn't matter in this context
 
-function judgeProgressiveness1D(arr = []) {
+function judgeProgressiveness1D( arr = [] ) {
 
     let averages = [], variances = []
-    
-    for (let arrLength = 2; arrLength < arr.length; arrLength++) {
 
-        for (let position = 0; position < arr.length - arrLength; position++) {
+    for ( let arrLength = 2; arrLength < arr.length; arrLength++ ) {
 
-            sliceToAnalyze = arr.slice(position, position + arrLength)
-            let average = calcAverage(sliceToAnalyze)
-            averages.push(average)
-            variances.push(calcVariance(sliceToAnalyze, average))
+        for ( let position = 0; position < arr.length - arrLength; position++ ) {
+
+            sliceToAnalyze = arr.slice( position, position + arrLength )
+            let average = calcAverage( sliceToAnalyze )
+            averages.push( average )
+            variances.push( calcVariance( sliceToAnalyze, average ) )
 
         }
 
     }
 
-    let progressiveness = calcVariance(averages, calcAverage(averages)) + calcVariance(variances, calcAverage(variances))
-    progressiveness     = 1 / (progressiveness * 5 + 1);
+    let progressiveness = calcVariance( averages, calcAverage( averages ) ) + calcVariance( variances, calcAverage( variances ) )
+    progressiveness = 1 / ( progressiveness * 5 + 1 )
     return progressiveness
 }
 
-function calcAverage2D(arr) { average = arr.reduce((sum, curr) => [sum[0]+curr[0],sum[1]+curr[1]], [0,0]); return [average[0]/arr.length, average[1]/arr.length] }
-function calcVariance2D(arr, average) { variance = arr.reduce((sum, curr) => [sum[0]+((curr[0]-average[0])*(curr[0]-average[0])), sum[1]+((curr[1]-average[1])*(curr[1]-average[1]))], [0,0]); return [variance[0]/arr.length, variance[1]/arr.length] }
+function calcAverage2D( arr ) { average = arr.reduce( ( sum, curr ) => [sum[0] + curr[0], sum[1] + curr[1]], [0, 0] ); return [average[0] / arr.length, average[1] / arr.length] }
+function calcVariance2D( arr, average ) { variance = arr.reduce( ( sum, curr ) => [sum[0] + ( ( curr[0] - average[0] ) * ( curr[0] - average[0] ) ), sum[1] + ( ( curr[1] - average[1] ) * ( curr[1] - average[1] ) )], [0, 0] ); return [variance[0] / arr.length, variance[1] / arr.length] }
 
-function calcVariance2DSqrtWeight(arr, average) { variance = arr.reduce((sum, curr, i) => [sum[0]+(Math.sqrt(i+1)*(curr[0]-average[0])*(curr[0]-average[0])), sum[1]+(Math.sqrt(i+1)*(curr[1]-average[1])*(curr[1]-average[1]))], [0,0]); return [variance[0]/arr.length, variance[1]/arr.length] }
+function calcVariance2DSqrtWeight( arr, average ) { variance = arr.reduce( ( sum, curr, i ) => [sum[0] + ( Math.sqrt( i + 1 ) * ( curr[0] - average[0] ) * ( curr[0] - average[0] ) ), sum[1] + ( Math.sqrt( i + 1 ) * ( curr[1] - average[1] ) * ( curr[1] - average[1] ) )], [0, 0] ); return [variance[0] / arr.length, variance[1] / arr.length] }
 // The respective scaling factor wouldn't actually be 1/arr.length but arr.length**1.5/1.5 - 1/1.5, but it doesn't matter in this context
 
-function judgeProgressiveness2D(arr = [[]]) {
+function judgeProgressiveness2D( arr = [[]] ) {
 
     let averages = [], variances = []
 
-    for (let arrLength = 2; arrLength <= arr.length; arrLength++) {
+    for ( let arrLength = 2; arrLength <= arr.length; arrLength++ ) {
 
-        for (let position = 0; position <= arr.length - arrLength; position++) {
+        for ( let position = 0; position <= arr.length - arrLength; position++ ) {
 
-            sliceToAnalyze = arr.slice(position, position + arrLength)
-            average = calcAverage2D(sliceToAnalyze)
-            averages.push(average)
-            variances.push(calcVariance2D(sliceToAnalyze, average).reduce((sum, curr) => sum+curr, 0)) // Sum up both x and y variances
+            sliceToAnalyze = arr.slice( position, position + arrLength )
+            average = calcAverage2D( sliceToAnalyze )
+            averages.push( average )
+            variances.push( calcVariance2D( sliceToAnalyze, average ).reduce( ( sum, curr ) => sum + curr, 0 ) ) // Sum up both x and y variances
 
         }
 
@@ -120,8 +120,8 @@ function judgeProgressiveness2D(arr = [[]]) {
 
     //console.log(averages, variances)
 
-    let progressiveness = calcVariance2D(averages, calcAverage2D(averages)).reduce((sum, curr) => sum+curr, 0) + calcVariance(variances, calcAverage(variances))
-    progressiveness     = 1 / (progressiveness * 5 + 1);
+    let progressiveness = calcVariance2D( averages, calcAverage2D( averages ) ).reduce( ( sum, curr ) => sum + curr, 0 ) + calcVariance( variances, calcAverage( variances ) )
+    progressiveness = 1 / ( progressiveness * 5 + 1 )
     return progressiveness
 }
 
@@ -136,29 +136,29 @@ function judgeProgressiveness2D(arr = [[]]) {
 
 
 
-function randInt(from = 0, to = 1) { // Returns a random Integer from from (including) to to (excluding)
-    return Math.round(Math.random() * (to - from - 1)) + from
+function randInt( from = 0, to = 1 ) { // Returns a random Integer from from (including) to to (excluding)
+    return Math.round( Math.random() * ( to - from - 1 ) ) + from
 }
 
-function improveProgressiveness1D(arr = [], steps = 1000) {
+function improveProgressiveness1D( arr = [], steps = 1000 ) {
 
     let progressiveness
-    for (let i = 0; i < steps; i++) {
+    for ( let i = 0; i < steps; i++ ) {
 
-        progressiveness = judgeProgressiveness1D(arr)
+        progressiveness = judgeProgressiveness1D( arr )
 
-        let pos1 = randInt(0, arr.length)
-        let pos2 = randInt(0, arr.length)
-        
+        let pos1 = randInt( 0, arr.length )
+        let pos2 = randInt( 0, arr.length )
+
         let tmp = arr[pos1]
         arr[pos1] = arr[pos2]
         arr[pos2] = tmp
 
-        let newProgressiveness = judgeProgressiveness1D(arr)
+        let newProgressiveness = judgeProgressiveness1D( arr )
 
         // If the progressiveness improved, keep array and set the new progressiveness
         // If not, swap back
-        if (newProgressiveness > progressiveness) {
+        if ( newProgressiveness > progressiveness ) {
             progressiveness = newProgressiveness
         } else {
             let tmp = arr[pos1]
@@ -172,28 +172,28 @@ function improveProgressiveness1D(arr = [], steps = 1000) {
 
 }
 
-function improveProgressiveness2D(arr = [[]], steps = 100, loadingBarId = undefined) {
+function improveProgressiveness2D( arr = [[]], steps = 100, loadingBarId = undefined ) {
 
     let loadingBar
-    if (loadingBarId != undefined) loadingBar = document.getElementById(loadingBarId)
+    if ( loadingBarId != undefined ) loadingBar = document.getElementById( loadingBarId )
 
-    let progressiveness = judgeProgressiveness2D(arr)
-    for (let i = 0; i < steps; i++) {
+    let progressiveness = judgeProgressiveness2D( arr )
+    for ( let i = 0; i < steps; i++ ) {
 
         //progressiveness = judgeProgressiveness2D(arr)
 
-        let pos1 = randInt(0, arr.length)
-        let pos2 = randInt(0, arr.length)
-        
+        let pos1 = randInt( 0, arr.length )
+        let pos2 = randInt( 0, arr.length )
+
         let tmp = arr[pos1]
         arr[pos1] = arr[pos2]
         arr[pos2] = tmp
 
-        let newProgressiveness = judgeProgressiveness2D(arr)
+        let newProgressiveness = judgeProgressiveness2D( arr )
 
         // If the progressiveness improved, keep array and set the new progressiveness
         // If not, swap back
-        if (newProgressiveness > progressiveness) {
+        if ( newProgressiveness > progressiveness ) {
             progressiveness = newProgressiveness
         } else {
             let tmp = arr[pos1]
@@ -211,8 +211,8 @@ function improveProgressiveness2D(arr = [[]], steps = 100, loadingBarId = undefi
 
     }
 
-    if (loadingBarId != undefined) loadingBar.style.width = "100%"
-    updateChart(arr);
+    if ( loadingBarId != undefined ) loadingBar.style.width = "100%"
+    updateChart( arr )
 
     return progressiveness
     //return arr //Array is modified in-place since js works with pointers
@@ -232,22 +232,24 @@ function improveProgressiveness2D(arr = [[]], steps = 100, loadingBarId = undefi
 
 
 
-function optimizeArray(str = "", steps = 100) {
-    let varname = str.match(/(vec|float)[2-4](\[[0-9]*\])?\s+(\w+)(\[[0-9]*\])?/i)[3] // Extract variable name
-    varname     = varname.replace(/_progressive\b/i, "")
-    let values  = multilang_parse_vec2_array(str)
+function optimizeArray( str = "", steps = 100 ) {
+    let varname = str.match( /(vec|float)[2-4](\[[0-9]*\])?\s+(\w+)(\[[0-9]*\])?/i )[3] // Extract variable name
+    varname = varname.replace( /_progressive\b/i, "" )
+    let values = multilang_parse_vec2_array( str )
 
-    let maxSampleDistance = values.reduce((prev, curr) => Math.max(prev, Math.abs(curr[0]), Math.abs(curr[1])), 0)
-    maxSampleDistance     = Math.max(maxSampleDistance * 1.1, 0.1);
+    let maxSampleDistance = values.reduce( ( prev, curr ) => Math.max( prev, Math.abs( curr[0] ), Math.abs( curr[1] ) ), 0 )
+    maxSampleDistance = Math.max( maxSampleDistance * 1.1, 0.1 )
     chart.options.scales.x.min = -maxSampleDistance
-    chart.options.scales.x.max =  maxSampleDistance
+    chart.options.scales.x.max = maxSampleDistance
     chart.options.scales.y.min = -maxSampleDistance
-    chart.options.scales.y.max =  maxSampleDistance
+    chart.options.scales.y.max = maxSampleDistance
     chart.update()
 
-    let progressiveness = improveProgressiveness2D(values, steps, "progress")
-    document.getElementById("output_progressiveness").innerHTML = `Progressiveness: ${progressiveness}`
+    if ( !!steps ) {
+        let progressiveness = improveProgressiveness2D( values, steps, "progress" )
+        document.getElementById( "output_progressiveness" ).innerHTML = `Progressiveness: ${progressiveness}`
+    }
 
-    document.getElementById("output").innerHTML = html_code(glsl_array(values, varname + "_progressive"))
+    document.getElementById( "output" ).innerHTML = html_code( glsl_array( values, varname + "_progressive" ) )
     //console.log(values)
 }

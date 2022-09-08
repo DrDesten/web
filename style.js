0e80aecab1aa39178a14fe8723a03da207965d24
DrDesten <>
2022-09-08 15:50:00 +0200

    improved noscript

--- style.js
+++ style.js
@@ -44,14 +44,21 @@ window.addEventListener("scroll", e => {
 // PARAGRAPH SECTIONS /////////////////////////////////////////////////////////////////////////
 
 // Add Alternating Colors to sections
-var paragraph_elements = $("section.paragraph")
-for (let i = 0; i < paragraph_elements.length; i++) {
-  if (i % 2 == 0) {
-    paragraph_elements[i].classList.add("color-variation-1")
-  } else {
-    paragraph_elements[i].classList.add("color-variation-1")
-  }
-}
+/* const c = [
+  {h: 202, s: 35, l: 35},
+  {h: 207, s: 40, l: 45},
+  {h: 226, s: 30, l: 45},
+  {h: 337, s: 60, l: 50},
+  {h: 110, s: 50, l: 50},
+]
+let index = Math.round(Math.random() * 2**53) % c.length
+document.querySelectorAll("section.paragraph").forEach(ele => {
+  const color = c[~~index]
+  ele.style.background = `linear-gradient(165deg, hsla(${color.h},${color.s}%,${color.l}%,0.85) 0%, hsl(${(color.h + 40) % 360},${color.s + 10}%,${color.l - 5}%,0.85) 100%)`
+  index = (index + 0.6180339887498948 * c.length) % c.length
+}) */
+document.querySelectorAll("section.paragraph").forEach(ele => ele.classList.add("color-variation-1"))
+
 
 /* Function to update amount of rows in textareas automatically
 ** <textarea/> "row" attribute sets the minimum amount of rows, while "maxrows" sets the maximum amount of rows */

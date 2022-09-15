8716483bf978f9b3b775e2ff510bb7a5a4b6f5d3
DrDesten <>
2022-09-15 16:17:54 +0200

    stuff

--- animation.js
+++ animation.js
@@ -8,7 +8,7 @@ if (!isTouchDevice) {
     const [rx,ry] = [x / window.innerWidth - .5, y / window.innerHeight - .5]
     for (const ele of floatingElements) {
       const factor = -ele.getAttribute("float-depth")
-      const displace = {x: factor * rx, y: factor * ry}
+      const displace = { x: factor * rx, y: factor * ry }
       ele.style.transform = `translate(${displace.x.toFixed(10)}rem, ${displace.y.toFixed(10)}rem)`
     }
   }

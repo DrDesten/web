d1ee0312f1b3836d3d596c682403e639be2436c4
DrDesten <70536771+DrDesten@users.noreply.github.com>
2024-07-26 18:14:34 +0000

    added touch handling to Mouse class

--- src/projects/fractals/mouse.js
+++ src/projects/fractals/mouse.js
@@ -15,11 +15,19 @@ class MouseHook {
             middle: false,
         }
 
+        /** @type {Map<number,{touch:Touch,history:Touch[]}>} */
+        this.touches = new Map()
+
         const eventTarget = this.element instanceof Window ? document : this.element
-        eventTarget.addEventListener( "mouseenter", e => this.movement( e ) )
-        eventTarget.addEventListener( "mousemove", e => this.movement( e ) )
-        eventTarget.addEventListener( "mousedown", e => this.button( e ) )
-        eventTarget.addEventListener( "mouseup", e => this.button( e ) )
+        eventTarget.addEventListener( "mouseenter", e => this.mouseMovement( e ) )
+        eventTarget.addEventListener( "mousemove", e => this.mouseMovement( e ) )
+        eventTarget.addEventListener( "mousedown", e => this.mouseButton( e ) )
+        eventTarget.addEventListener( "mouseup", e => this.mouseButton( e ) )
+
+        eventTarget.addEventListener("touchstart", e => this.touchStart(e))
+        eventTarget.addEventListener("touchmove", e => this.touchChange(e))
+        eventTarget.addEventListener("touchend", e => this.touchEnd(e))
+        eventTarget.addEventListener("touchcancel", e => this.touchEnd(e))
     }
 
     stats() {
@@ -30,20 +38,45 @@ class MouseHook {
     }
 
     /** @param {MouseEvent} event */
-    button( event ) {
+    mouseButton( event ) {
+        console.log("mouse click")
         this.buttons.primary = !!( event.buttons & 1 )
         this.buttons.secondary = !!( event.buttons & 2 )
         this.buttons.middle = !!( event.buttons & 4 )
     }
 
     /** @param {MouseEvent} event */
-    movement( event ) {
+    mouseMovement( event ) {
+        console.log("mouse movement")
         const { x, y, width, height } = this.stats()
         const { clientX: mouseX, clientY: mouseY } = event
         this.position.set( mouseX - x, mouseY - y )
         this.relativePosition.set( this.position.x / width, this.position.y / height )
         this.webglPosition.set( this.position.x, height - this.position.y )
         this.relativeWebglPosition.set( this.relativePosition.x, 1 - this.relativePosition.y )
+    }
+
+    /** @param {TouchEvent} event */
+    touchStart( event ) {
+        for ( const touch of event.changedTouches ) {
+            this.touches.set( touch.identifier, { touch, history: [touch] } )
+        }
+    }
+
+    /** @param {TouchEvent} event */
+    touchChange( event ) {
+        for ( const touch of event.touches ) {
+            const object = this.touches.get(touch.identifier)
+            object.touch = touch 
+            object.history.push( touch )
+        }
+    }
+
+    /** @param {TouchEvent} event */
+    touchEnd( event ) {
+        for ( const touch of event.changedTouches ) {
+            this.touches.delete( touch.identifier )
+        }
     }
 }
 
